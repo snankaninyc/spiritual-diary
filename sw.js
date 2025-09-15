958d7e3e0796b8aff988b5c634d044da2a537cd8
snankaninyc <snankani@gmail.com>
2025-09-14 21:40:19 -0400

    Add files via upload
    Add spiritual diary PWA

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'spiritual-diary-v1';
+const urlsToCache = [
+  '/',
+  '/spiritual-diary.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/logo sseducare.png',
+  '/SSSE-logo.png',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
